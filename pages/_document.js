eb8dc0c81f2b7d7b0adae6947d71254e5d1525eb
Naresh E <nareshe64@gmail.com>
2022-07-14 15:07:06 +0530

    document head changed

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,44 @@
-import Document,{ Head,Html,Main,NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
+    let overview ="ZFlix is the largest free streaming platform for movies and tv shows. Collaborative media and info service featuring high quality content for a huge selection of titles and new releases! Available in all countries.";
+    const base_url="https://zflix-app.netlify.app/en"
     return (
       // ...
       <Html>
         <Head>
-        <link rel='manifest' href='/manifest.json' />
+          <title>ZFlix - Watch Movies & TV Shows</title>
+          <meta name="title" content={"ZFlix - Watch Movies & TV Shows"} />
+          <meta name="description" content={overview} />
+          <meta
+            name="keywords"
+            content="Movies, TV Shows, Streaming, Reviews, Actors, Actresses, Photos, User Ratings, Synopsis, Trailers, Teasers, Credits, Cast"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={base_url} />
+          <meta property="og:site_name" content="ZFlix" />
+          <meta
+            property="og:title"
+            content={"ZFlix - Watch Movies & TV Shows"}
+          />
+          <meta property="og:description" content={overview} />
+          <meta property="og:image" content="/icons/apple-touch-icon.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content={base_url} />
+          <meta
+            property="twitter:title"
+            content={"ZFlix - Watch Movies & TV Shows"}
+          />
+          <meta property="twitter:description" content={overview} />
+          <meta
+            property="twitter:image"
+            content="/icons/apple-touch-icon.png"
+          ></meta>
+
+          <link rel="manifest" href="/manifest.json" />
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-8FMMTY6M6W"
@@ -24,7 +56,7 @@ export default class MyDocument extends Document {
           />
         </Head>
         <Main />
-        <NextScript  />
+        <NextScript />
       </Html>
     );
   }
