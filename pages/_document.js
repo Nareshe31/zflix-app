83b4945cd1fcac5f2f2966ef1398887c7073ea92
Naresh E <nareshe64@gmail.com>
2022-07-31 11:24:22 +0530

    google tag manager added

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,10 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <meta name="google-site-verification" content="Y18D4bzANlD1TP1aLl21MutvmIAcf8QIIwnmjhVfZ1o" />
+          <meta
+            name="google-site-verification"
+            content="Y18D4bzANlD1TP1aLl21MutvmIAcf8QIIwnmjhVfZ1o"
+          />
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-8FMMTY6M6W"
@@ -23,6 +26,21 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+          {/* <!-- Google Tag Manager --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){
+              w[l] = w[l] || [];
+              w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N4N8H7V');`,
+            }}
+          />
+          {/* <!-- End Google Tag Manager --> */}
         </Head>
         <Main />
         <NextScript />
