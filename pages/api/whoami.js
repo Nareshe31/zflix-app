ba0da25919a5824fe85c31fcd0b55b08e8650d0e
Naresh E <nareshe64@gmail.com>
2022-08-28 21:27:08 +0530

    Cors added

---
+++ pages/api/whoami.js
@@ -0,0 +1,15 @@
+import NextCors from 'nextjs-cors';
+
+async function handler(req, res) {
+   // Run the cors middleware
+   // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+   await NextCors(req, res, {
+      // Options
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: 'https://zflix-app.netlify.app',
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+   });
+
+   // Rest of the API logic
+   res.json({ message: 'Hello NextJs Cors!' });
+}
