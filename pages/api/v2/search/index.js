37a333673e13659d81f703bd99256c7f191668e2
Naresh <nareshe64@gmail.com>
2022-09-11 11:48:43 +0530

    Updated search api

--- pages/api/v2/search/index.js
+++ pages/api/v2/search/index.js
@@ -9,11 +9,11 @@ export default async function handler(req, res) {
             `https://api.themoviedb.org/3/search/multi?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
         );
         var data=await response.json()
-        res.send({...data,query});
+        res.send({...data,query,success:true});
         }
        
     catch (error) {
         console.log(error.message);
-        res.status(400).send({ message: "An error occurred, please try again later" });
+        res.status(400).send({ message: "An error occurred, please try again later",error:error.message });
     }
 }
