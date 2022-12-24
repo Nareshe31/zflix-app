ff21d2b2fbe9c0a4df4785d966dce2bd3fe60a3c
Naresh E <nareshe64@gmail.com>
2022-12-24 17:37:39 +0530

    added buy me a coffee

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,9 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+
+          {/*Buy me a coffee*/}
+          <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="naresh31" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
           {/* <!-- Google Tag Manager --> */}
           {/* <script
             dangerouslySetInnerHTML={{
